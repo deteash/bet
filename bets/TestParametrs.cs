ee473b535132a9063cf2cdfa7eb01a067c866181
Deteash <galtravi@inbox.ru>
2018-08-09 03:38:51 +0400

    reworked storing struc, added autotest, bugfix, reading optimisation

---
+++ bets/TestParametrs.cs
@@ -0,0 +1,22 @@
+﻿namespace bets
+{
+    class TestParametrs
+    {
+        public int bets;
+        public int btimes;
+        public int hoursFrom;
+        public int hoursTo;
+        public double daniloCoeff;
+        public bool p1p2x;
+        public bool fora;
+        public bool total;
+        public bool moreBets;
+        public bool noDraw;
+        public bool freshLine;
+        public bool daniloStyle;
+        public bool Naverochku;
+        public bool ordinaryBets;
+
+
+    }
+}
